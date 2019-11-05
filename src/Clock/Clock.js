9ec7a8500c50e63e8285eed2aeb086d5488c49a4
Rafa <zoldan.rafael@gmail.com>
2019-11-05 14:25:29 +0200

    Made dynamic clock

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -7,11 +7,37 @@ import React, { Component } from 'react';
  */
 
 class Clock extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			time: new Date()
+		}
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+
+	tick() {
+		this.setState({
+			time: new Date()
+		});
+	}
+
+
 	render() {
 		return (
 			<div>
 				<h1>Clock</h1>
-				The time is:
+				The time is: {this.state.time.toLocaleTimeString()}
 			</div>
 		);
 	}
