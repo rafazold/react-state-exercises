188d3c2d51f969aa674e776a21ea9ad6febaef4f
Rafa <zoldan.rafael@gmail.com>
2019-11-05 13:21:47 +0200

    Added values from inputs to reservation text

--- src/FoodPicker/FoodPicker.js
+++ src/FoodPicker/FoodPicker.js
@@ -6,17 +6,39 @@ import React, { Component } from 'react';
  */
 
 class FoodPicker extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			restaurant: 'Frangelico',
+			meal: '',
+			desert: ''
+		}
+	}
+
+	getInput(e) {
+		if(e.target.type === "checkbox"){
+			this.setState({
+				[e.target.name]: e.target.checked,
+			});
+		} else {
+			this.setState({
+				[e.target.name]: e.target.value,
+			});
+		}
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Order special meal:</h1>
 				<div>
 					Your name:
-					<input type="text" />
+					<input type="text" name="name" onInput={this.getInput.bind(this)} />
 				</div>
 				<div>
 					Choose restaurant:
-					<select>
+					<select name="restaurant" onChange={this.getInput.bind(this)}>
 						<option value="Frangelico">Frangelico</option>
 						<option value="Mina Tomei">Mina Tomei</option>
 						<option value="Rak Basar">Rak Basar</option>
@@ -25,19 +47,20 @@ class FoodPicker extends Component {
 				</div>
 				<div>
 					Favorite meal:
-					<input type="text" />
+					<input type="text" name="meal" onInput={this.getInput.bind(this)}/>
 				</div>
 				<div>
 					Want a desert?
-					<input type="checkbox" />
+					<input type="checkbox" name="desert" onChange={this.getInput.bind(this)}/>
 				</div>
 
 				<div>
 					<h2>Your reservation:</h2>
-					Hi [NAME]! <br />
-					We are glad you want to reserve a table at [RESTAURANT].<br />
-					We will make sure that your favorite meal, [MEAL] is available.<br />
+					Hi [{this.state.name}]! <br />
+					We are glad you want to reserve a table at [{this.state.restaurant}].<br />
+					We will make sure that your favorite meal, [{this.state.meal}] is available.<br />
 					Additionally, our chef will make a special desert for you!<br />
+					{this.state.desert ? "Thank you for the desert" : "No desert today..."}
 				</div>
 			</div>
 		);
